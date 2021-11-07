430c23dabe93f7fe2311889ddbae00d01e88b82e
SNEHABHIxd <93216312+SNEHABHIxd@users.noreply.github.com>
2021-11-07 21:06:54 +0530

    Create mute_db.py

---
+++ pySNEHABHI/dB/mute_db.py
@@ -0,0 +1,34 @@
+from .. import udB
+
+
+def get_stuff():
+    a = udB.get("MUTE")
+    if not a:
+        return {}
+    try:
+        return eval(a)
+    except BaseException:
+        udB.delete("MUTE")
+    return {}
+
+
+def mute(chat, id):
+    ok = get_stuff()
+    if ok.get(chat):
+        if id not in ok[chat]:
+            ok[chat].append(id)
+    else:
+        ok.update({chat: [id]})
+    udB.set("MUTE", str(ok))
+
+
+def unmute(chat, id):
+    ok = get_stuff()
+    if ok.get(chat) and id in ok[chat]:
+        ok[chat].remove(id)
+    udB.set("MUTE", str(ok))
+
+
+def is_muted(chat, id):
+    ok = get_stuff()
+    return bool(ok.get(chat) and id in ok[chat])
