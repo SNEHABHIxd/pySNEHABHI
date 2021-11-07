b583918ecd5dc6cef49a32f11d0e592ddff2826c
SNEHABHIxd <93216312+SNEHABHIxd@users.noreply.github.com>
2021-11-07 21:10:24 +0530

    Create pmpermit_db.py

---
+++ pySNEHABHI/dB/pmpermit_db.py
@@ -0,0 +1,43 @@
+from .. import udB
+
+
+def get_stuff():
+    a = udB.get("PMPERMIT")
+    if not a:
+        return []
+    try:
+        return eval(a)
+    except BaseException:
+        try:
+            # Transferring stuff From old format to new
+            x, y = [], udB.get("PMPERMIT").split()
+            for z in y:
+                x.append(int(z))
+            udB.set("PMPERMIT", str(x))
+            return x
+        except BaseException:
+            pass
+    return []
+
+
+def get_approved():
+    return get_stuff()
+
+
+def approve_user(id):
+    ok = get_approved()
+    if id not in ok:
+        ok.append(id)
+        udB.set("PMPERMIT", str(ok))
+
+
+def disapprove_user(id):
+    ok = get_approved()
+    if id in ok:
+        ok.remove(id)
+        udB.set("PMPERMIT", str(ok))
+
+
+def is_approved(id):
+    ok = get_approved()
+    return id in ok
